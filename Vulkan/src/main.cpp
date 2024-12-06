ffe643dfc1c3e5597946fb969d1cbfe3f12c6120
lhjack <lhjackgod@163.com>
2024-12-06 20:42:52 +0800

    create vulkan instance

--- Vulkan/src/main.cpp
+++ Vulkan/src/main.cpp
@@ -26,28 +26,57 @@ public:
 private:
     void initVulKan()
     {
-        glfwInit();
-
-        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-        m_Window = glfwCreateWindow(WIDTH, HEIGHT, "VulKan", nullptr, nullptr);
+        createInstance();
     }
     void mainLoop()
     {
-        while (!glfwWindowShouldClose(m_Window))
-        {
-            glfwPollEvents();
-        }
+       
     }
     void cleanup()
     {
-        glfwDestroyWindow(m_Window);
-        glfwTerminate();
+        
+    }
+    void createInstance()
+    {
+        //the message in vulkan deliver through struct
+        VkApplicationInfo appinfo{};
+        appinfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appinfo.pApplicationName = "hello triangle";
+        appinfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appinfo.pEngineName = "no Engine";
+        appinfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appinfo.apiVersion = VK_API_VERSION_1_0;
+        //createInstace info
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appinfo;
+        //this two variable.
+        //the glfwExtensionCount is the glfw need extension count
+        //glfwExtensions are the glfw need extension name
+        //is all about glfw no vulkan
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        //follow variables are vulkan create a instace needs
+        //not only have the glfwExtensions, but in this case only has the glfwExtension
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        //now we have the necessary info for creating instance
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &m_vulkanInstance);
+        if (result != VK_SUCCESS)
+        {
+            throw std::runtime_error("error to create instance!");
+        }
     }
 private:
     const uint32_t WIDTH = 800;
     const uint32_t HEIGHT = 600;
     GLFWwindow* m_Window;
+    VkInstance m_vulkanInstance;
 };
 
 int main() {

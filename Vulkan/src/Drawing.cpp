28c65a9bb955e4acb498f11035b675c13bac5ec5
lhjack <lhjackgod@163.com>
2024-12-23 18:00:28 +0800

    add way to align the data

--- Vulkan/src/Drawing.cpp
+++ Vulkan/src/Drawing.cpp
@@ -1293,9 +1293,9 @@ private:
 
 	struct UniformBufferObject
 	{
-		glm::mat4 model;
-		glm::mat4 view;
-		glm::mat4 perspective;
+		alignas(16) glm::mat4 model;
+		alignas(16) glm::mat4 view;
+		alignas(16) glm::mat4 perspective;
 	};
 
 	std::vector<Vertex> vertices = {
